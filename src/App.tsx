30d392e83be7a3ccc70ecb890dd98c503591f631
MoizRed <abdoureduniv2000@gmail.com>
2024-11-02 19:51:12 +0100

    h

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// @deno-types="@types/react"
+import { useState } from 'react'
+
+// @deno-types="@types/react-dom/client"
+
+function App() {
+
+
+  return (
+  <>
+  <h1>hello hahahaha</h1>
+    </>
+  )
+}
+
+export default App
